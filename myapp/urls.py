879749c2a14d0a62d9e38d076b46a2e744ce3eed
MgZayarNaing <zayarnaing.st@gmail.com>
2024-09-03 11:12:31 +0630

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('task_list/', views.task_list, name='task_list'),
+    path('task_create/', views.task_create, name='task_create'),
+    path('task_delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('task_update/<int:task_id>/', views.task_update, name='task_update'),
+
+]
---
+++ myapp/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from myapp.models import Task
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login,logout
+# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+
+
+@login_required(login_url="login")
+def task_create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        is_completed = request.POST.get('is_completed') == 'on'
+        task = Task(title=title, description=description, due_date=due_date, is_completed=is_completed)
+        task.save()
+
+        messages.success(request, 'Task created successfully')
+
+        return redirect('task_list')
+    return render(request, 'task_form.html')
+
+
+@login_required(login_url="login")
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.delete()
+        messages.success(request, 'Task delete successfully')
+        return redirect('task_list')
+
+    return render(request, 'delete_task.html', {'task': task})
+
+@login_required(login_url="login")
+def task_update(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description')
+        task.due_date = request.POST.get('due_date')
+        task.is_completed = request.POST.get('is_completed') == 'on'
+
+        task.save()
+        messages.success(request, 'Task update successfully')
+        return redirect('task_list')
+
+    return render(request, 'task_form.html', {'task': task})
+
+
+
+def login_view(request):
+    if request.method  == 'GET':
+        return render(request, 'login.html')
+    if request.method  == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('task_list')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login/')
